03e334bcd13213315938a7d0ff4ae29eb182cb1e
Oleksii Matyko <alexa.ua@gmail.com>
2025-04-09 23:38:12 +0300

    MailConfig and AppConfig classes added

---
+++ MailConfig.cs
@@ -0,0 +1,15 @@
+﻿public class MailConfig
+    {
+        public required string Username { get; set; }
+        public required string AppPassword { get; set; }
+        public required string Subject { get; set; }
+        public required string Body { get; set; }
+        public required string Alias { get; set; }
+}
+
+public class AppConfig
+{
+    public required MailConfig Email { get; set; }
+    public required string FolderPath { get; set; }
+    public required string Attachment { get; set; }
+}
--- Program.cs
+++ Program.cs
@@ -6,6 +6,8 @@ using System.Net.Mail;
 using System.Net;
 using ExcelDataReader;
 using Microsoft.Extensions.Configuration;
+using System.Diagnostics;
+
 
 class Program
 {
@@ -13,52 +15,110 @@ class Program
     {
         System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
 
-        var config = LoadConfig();
-        string emailUser = config["Email:User"];
-        string emailPassword = config["Email:Password"];
-        string folderPath = config["FolderPath"];
+        var appConfig = LoadAppConfig();
+        var mail = appConfig.Email;
+        string folderPath = appConfig.FolderPath;
+        string attachment = appConfig.Attachment;
+
+        if (string.IsNullOrWhiteSpace(mail.Username) ||
+        string.IsNullOrWhiteSpace(mail.AppPassword) ||
+        string.IsNullOrWhiteSpace(mail.Subject) ||
+        string.IsNullOrWhiteSpace(mail.Body) ||
+        string.IsNullOrWhiteSpace(folderPath))
+        {
+            Console.WriteLine("❌ Missing configuration in appsettings.json.");
+            Environment.Exit(1);
+        }
+
+        
+
         string clientsFile = Path.Combine(folderPath, "clients.xlsx");
-        //Console.WriteLine(clientsFile);
-
         List<string> emails = GetEmailsFromExcel(clientsFile);
-        //Console.WriteLine($"Найдено адресов: {emails.Count}");
+        string logFile = Path.Combine(folderPath, "log.txt");
+        int count = 0;
 
-        string priceFile = GetLatestPriceFile(folderPath);
+        string priceFile = GetLatestPriceFile(folderPath, attachment);
         Console.WriteLine(priceFile);
 
+        var nextSendTime = DateTime.Now;
         foreach (string email in emails)
         {
             try
             {
-                SendEmailWithAttachment(email, priceFile, emailUser, emailPassword);
-                Console.WriteLine($"Письмо отправлено: {email}");
-                Thread.Sleep(12000);
+                var now = DateTime.Now;
+                if (now < nextSendTime)
+                {
+                    var waitTime = nextSendTime - now;
+                    Thread.Sleep(waitTime);
+                }
+                var stopwatch = Stopwatch.StartNew();
+
+                SendEmailWithAttachment(email, priceFile, appConfig);
+                stopwatch.Stop();
+
+                string logMessage = $"{DateTime.Now:yyyy-MM-dd HH:mm:ss} OK     → {email}  {stopwatch.Elapsed.TotalSeconds:F1}s";
+                File.AppendAllText(logFile, logMessage + Environment.NewLine);
+                Console.WriteLine(logMessage);
+                nextSendTime = DateTime.Now.AddSeconds(12 - stopwatch.Elapsed.TotalSeconds);
             }
             catch (Exception ex)
             {
-                Console.WriteLine($"Ошибка при отправке на {email}: {ex.Message}");
+                string logMessage = $"{DateTime.Now:yyyy-MM-dd HH:mm:ss} ERROR     → {email}: {ex.Message}";
+                File.AppendAllText(logFile, logMessage + Environment.NewLine);
+                Console.WriteLine(logMessage);
             }
+
+            count++;
+
+            if (count % 100 == 0)
+            {
+                Console.WriteLine($"⏸ Pause 30 minutes after {count} emails...");
+                File.AppendAllLines(logFile, new[] { $"-- {DateTime.Now:yyyy-MM-dd HH:mm:ss} — PAUSE 30 min after {count} emails --" });
+                Thread.Sleep(30 * 60 * 1000);
+            }
+         
         }
 
-        Console.WriteLine("Рассылка завершена.");
+        Console.WriteLine("Done.");
 
     }
 
-    static IConfiguration LoadConfig()
+    static AppConfig LoadAppConfig()
     {
-        var builder = new ConfigurationBuilder()
+        var config = new ConfigurationBuilder()
             .SetBasePath(Directory.GetCurrentDirectory())
-            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
-        return builder.Build();
+            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+            .Build();
+
+        //AppConfig appConfig = new AppConfig();
+        //config.Bind(appConfig);
+        var email = new MailConfig
+        {
+            Alias = config["Email:Alias"],
+            Username = config["Email:Username"],
+            AppPassword = config["Email:AppPassword"],
+            Subject = config["Email:Subject"],
+            Body = config["Email:Body"]
+        };
+
+        return new AppConfig
+        {
+            FolderPath = config["FolderPath"],
+            Attachment = config["Attachment"],
+            Email = email
+        };
+
+     
     }
 
-    static string GetLatestPriceFile(string folderPath)
+    static string GetLatestPriceFile(string folderPath, string attachment)
     {
-        var files = Directory.GetFiles(folderPath, "price_nks13_*.xls");
+        var files = Directory.GetFiles(folderPath, attachment);
         if (files.Length == 0)
             throw new FileNotFoundException("File not found!");
         return files.OrderByDescending(File.GetLastWriteTime).First();
     }
+    
     static List<string> GetEmailsFromExcel(string filePath)
     {
         List<string> emails = new List<string>();
@@ -79,9 +139,9 @@ class Program
         return emails;
     }
 
-    static void SendEmailWithAttachment(string toEmail, string attachmentPath, string emailUser, string emailPass)
+    static void SendEmailWithAttachment(string toEmail, string attachmentPath, AppConfig appConfig)
     {
-        var fromAddress = new MailAddress(emailUser, "Alex from NKS13");
+        var fromAddress = new MailAddress(appConfig.Email.Username, appConfig.Email.Alias);
         var toAddress = new MailAddress(toEmail);
 
         var smtp = new SmtpClient
@@ -89,13 +149,13 @@ class Program
             Host = "smtp.gmail.com",
             Port = 587,
             EnableSsl = true,
-            Credentials = new NetworkCredential(emailUser, emailPass)
+            Credentials = new NetworkCredential(appConfig.Email.Username, appConfig.Email.AppPassword)
         };
 
         using (var message = new MailMessage(fromAddress, toAddress)
         {
-            Subject = "Обновлённый прайс-лист",
-            Body = "Добрый день!\n\nВо вложении актуальный прайс-лист.\n\nС уважением,\nNKS13"
+            Subject = appConfig.Email.Subject,
+            Body = appConfig.Email.Body
         })
         {
             message.Attachments.Add(new Attachment(attachmentPath));
